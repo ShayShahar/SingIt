1040e4a6dcf3105d37a8a79277b65a6d8a8fcb99
ShayShahar <shayscal90@gmail.com>
2016-05-31 20:35:42 +0300

    update favorites row view

--- app/src/main/java/com/singit/shays/singit/LyricsViewActivity.java
+++ app/src/main/java/com/singit/shays/singit/LyricsViewActivity.java
@@ -10,6 +10,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -44,6 +46,28 @@ public class LyricsViewActivity extends AppCompatActivity {
         new DownloadImageTask(image)
                 .execute(lyrics.imageURL);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_lyrics_view, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_favorites) {
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 
     class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
         ImageView bmImage;

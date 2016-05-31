d672e72647e0d10dbdf695902b49e76d9e3e2654
Carmel Avni <carmelavni@walla.co.il>
2016-06-01 00:30:16 +0300

    Update the FavoritesViewActivity, add a message when the list is empty, adding the option to delete from favorites list (without implement in the DB)

--- app/src/main/java/com/singit/shays/singit/FavoritesViewActivity.java
+++ app/src/main/java/com/singit/shays/singit/FavoritesViewActivity.java
@@ -1,22 +1,35 @@
 package com.singit.shays.singit;
 
+import android.app.LauncherActivity;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
+/**
+ * The favorites activity logic
+ */
 public class FavoritesViewActivity extends AppCompatActivity {
 
     private SingItDBHelper dbHelper;
-
+    private ListView list;
+    private  ListAdapter adapter;
     @Override
+/**
+ * This method run when the activity is created and upload the favorites list
+ * @param savedInstanceState
+ */
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favorites_view);
@@ -25,10 +38,29 @@ public class FavoritesViewActivity extends AppCompatActivity {
 
         dbHelper = new SingItDBHelper(this);
         ArrayList<LyricsRes> favorites = dbHelper.get_favorite_songs();
-        ListAdapter adapter = new CustomAdapter(this,favorites);
-        ListView list = (ListView) findViewById(R.id.favoritesList);
+
+
+
+        adapter = new CustomAdapter(this,favorites);
+        list = (ListView) findViewById(R.id.favoritesList);
+        list.setEmptyView(findViewById(R.id.empty));
         list.setAdapter(adapter);
-
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> a, View v, final int position, long id) {
+                AlertDialog.Builder adb=new AlertDialog.Builder(FavoritesViewActivity.this);
+                adb.setTitle("Delete?");
+                LyricsRes item = (LyricsRes)list.getItemAtPosition(position);
+                adb.setMessage("Are you sure you want to delete " +item.artist+" "+item.title+"?" );
+                final int positionToRemove = position;
+                adb.setNegativeButton("Cancel", null);
+                adb.setPositiveButton("Ok", new AlertDialog.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        LyricsRes item = (LyricsRes)list.getItemAtPosition(position);
+                        //dbHelper.remove(item.id);
+                        adapter.notifyAll();
+                    }});
+                adb.show();
+            }
+        });
+        }
     }
-
-}

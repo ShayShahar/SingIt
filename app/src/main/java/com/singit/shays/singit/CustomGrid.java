cb4a9f06071a7af98b96809c9b3edb80c4be538d
ShayShahar <shayscal90@gmail.com>
2016-06-02 22:40:10 +0300

    update

--- app/src/main/java/com/singit/shays/singit/CustomGrid.java
+++ app/src/main/java/com/singit/shays/singit/CustomGrid.java
@@ -1,7 +1,109 @@
 package com.singit.shays.singit;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+
 /**
  * Created by shays on 02/06/2016.
  */
-public class CustomGrid {
+public class CustomGrid extends BaseAdapter {
+
+
+    private Context context;
+    private ArrayList<LyricsRes> result;
+    private TextView artist;
+    private TextView song;
+    private ImageView img;
+    private Bitmap bmp;
+
+
+    private static LayoutInflater inflater=null;
+    public CustomGrid(MainActivity mainActivity, ArrayList<LyricsRes> resArrayList) {
+        result=resArrayList;
+        context=mainActivity;
+        inflater = ( LayoutInflater )context.
+                getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        return result.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return position;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        View rowView;
+
+        rowView = inflater.inflate(R.layout.custom_grid, null);
+        artist =(TextView) rowView.findViewById(R.id.artistNameGrid);
+        song =(TextView) rowView.findViewById(R.id.songNameGrid);
+        img =(ImageView) rowView.findViewById(R.id.coverImage);
+
+        artist.setText(result.get(position).artist);
+        song.setText(result.get(position).title);
+
+        new DownloadImageTask(img)
+                .execute(result.get(position).imageURL);
+
+        rowView.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+               // Toast.makeText(context, "You Clicked "+result[position], Toast.LENGTH_LONG).show();
+            }
+        });
+
+        return rowView;
+    }
+
+
+
+    class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+
+        public DownloadImageTask(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap mIcon11 = null;
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+                bmp = mIcon11;
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            bmImage.setImageBitmap(result);
+        }
+    }
 }
--- app/src/main/java/com/singit/shays/singit/MainActivity.java
+++ app/src/main/java/com/singit/shays/singit/MainActivity.java
@@ -3,6 +3,7 @@ package com.singit.shays.singit;
 import android.app.SearchManager;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
@@ -16,6 +17,7 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.AdapterView;
+import android.widget.GridView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.ProgressBar;
@@ -27,6 +29,7 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private GridView gridView;
     private ProgressBar spinner;
     private TextView songName;
     private LyricsAPI api;
@@ -48,6 +51,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         toolbar = (Toolbar)findViewById(R.id.toolbar);
+        //toolbar.setTitleTextColor(Color.WHITE);
         setSupportActionBar(toolbar);
         this.db = new SingItDBHelper(this);
         setContentView(R.layout.activity_main);
@@ -56,18 +60,18 @@ public class MainActivity extends AppCompatActivity {
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.drawer_open,R.string.drawer_close);
         drawerLayout.setDrawerListener(actionBarDrawerToggle);
-        spinner = (ProgressBar)findViewById(R.id.progressBar1);
-        spinner.setVisibility(View.GONE);
-
-        Log.d(TAG, "onCreate() Start");
+       // spinner = (ProgressBar)findViewById(R.id.progressBar1);
+       // spinner.setVisibility(View.GONE);
+        gridView = (GridView) findViewById(R.id.gridView);
         ArrayList<LyricsRes> last_searches = db.get_last_searched_songs();
-        ListAdapter adapter = new CustomAdapter(this,last_searches);
-        final ListView list = (ListView) findViewById(R.id.lastSearchesList);
-        list.setAdapter(adapter);
+        //ListAdapter adapter = new CustomAdapter(this,last_searches);
+       // final ListView list = (ListView) findViewById(R.id.lastSearchesList);
+        gridView.setAdapter(new CustomGrid(this,last_searches));
 
       //  songName  = (TextView) findViewById(R.id.songNameTxt);
         api = new LyricsAPI();
 
+        /*
 
         AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
             @Override
@@ -91,7 +95,7 @@ public class MainActivity extends AppCompatActivity {
             }
         };
 
-        list.setOnItemClickListener(itemClickListener);
+        list.setOnItemClickListener(itemClickListener);*/
 
         Log.d(TAG, "onCreate() Finish");
     }

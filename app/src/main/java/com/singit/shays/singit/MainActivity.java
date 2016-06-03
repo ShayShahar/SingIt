106839aa19c8319017a28dafde67709f78a317e2
ShayShahar <shayscal90@gmail.com>
2016-06-03 14:58:17 +0300

    someone try to fix the navigation_drawer item select

--- app/src/main/java/com/singit/shays/singit/MainActivity.java
+++ app/src/main/java/com/singit/shays/singit/MainActivity.java
@@ -8,6 +8,8 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -28,7 +30,7 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     private GridView gridView;
     private SingItDBHelper db;
@@ -36,14 +38,88 @@ public class MainActivity extends AppCompatActivity {
     private DrawerLayout drawerLayout;
     private LyricsAPI api;
     private ActionBarDrawerToggle actionBarDrawerToggle;
+    private static final String TAG = "SingDebug";
+    private NavigationView navigationView;
 
-    public void setLyrics(String lyrics) {
-        this.lyrics = lyrics;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(TAG,"on create");
+        // Initializing Toolbar and setting it as the actionbar
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //Initializing NavigationView
+        navigationView = (NavigationView) findViewById(R.id.navigation_view);
+
+        //Setting Navigation View Item Selected Listener to handle the item click of the navigation menu
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                menuItem.setChecked(true);
+                switch (menuItem.getItemId()) {
+                    case R.id.home:
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        return true;
+                    // More items
+                }
+                return false;
+            }
+        });
+
+        // Initializing Drawer Layout and ActionBarToggle
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.drawer_open, R.string.drawer_close){
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                // Code here will be triggered once the drawer closes as we dont want anything to happen so we leave this blank
+                super.onDrawerClosed(drawerView);
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                // Code here will be triggered once the drawer open as we dont want anything to happen so we leave this blank
+
+                super.onDrawerOpened(drawerView);
+            }
+        };
+
+        //Setting the actionbarToggle to drawer layout
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+
+        //calling sync state is necessay or else your hamburger icon wont show up
+        actionBarDrawerToggle.syncState();
+
+
+        api = new LyricsAPI();
+        this.db = new SingItDBHelper(this);
+        gridView = (GridView) findViewById(R.id.gridView);
+        final ArrayList<LyricsRes> last_searches = db.get_last_searched_songs();
+        gridView.setAdapter(new CustomGrid(this,last_searches));
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+
+                LyricsRes selected = last_searches.get(position);
+                LyricsRes pass = selected;
+                try{
+                    pass = api.getLyrics(selected);
+                }catch(Exception e){
+                    Log.d(TAG,e.toString());
+                }
+                Intent intent = new Intent(MainActivity.this,LyricsViewActivity.class);
+                intent.putExtra("view",pass);
+                startActivity(intent);
+            }
+        });
     }
 
-    private String lyrics;
-    private static final String TAG = "SingDebug";
-
+    private boolean handleTheSelectedMenuItem (MenuItem item){
+        return false;
+    }
+/*
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,13 +129,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        api = new LyricsAPI();
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.drawer_open,R.string.drawer_close);
-        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
         gridView = (GridView) findViewById(R.id.gridView);
         final ArrayList<LyricsRes> last_searches = db.get_last_searched_songs();
         gridView.setAdapter(new CustomGrid(this,last_searches));
+        navigationView = (NavigationView)findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
+        Log.d(TAG,"navigationView SET");
 
         gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View v,
@@ -79,7 +159,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
         Log.d(TAG, "onCreate() Finish");
-    }
+    }*/
 
     public void onMoreSearchesButtonClick(View view){
 
@@ -87,11 +167,11 @@ public class MainActivity extends AppCompatActivity {
         this.startActivity(intent);
 
     }
-    @Override
+   /* @Override
     protected void onPostCreate(Bundle savedInstanceState){
         super.onPostCreate(savedInstanceState);
         actionBarDrawerToggle.syncState();
-    }
+    }*/
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -119,6 +199,26 @@ public class MainActivity extends AppCompatActivity {
         gridView.setAdapter(new CustomGrid(this,last_searches));
     }
 
+/*    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item){
+        Log.d(TAG,"onNavigationItemSelected");
+
+        switch(item.getItemId()){
+            case R.id.home:{
+                //return true;
+            }
+            case R.id.action_favorites:{
+                Log.d(TAG,"on action favorites");
+                //Intent intent = new Intent(MainActivity.this, FavoritesViewActivity.class);
+                //startActivity(intent);
+            }
+
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }*/
 
 
 }

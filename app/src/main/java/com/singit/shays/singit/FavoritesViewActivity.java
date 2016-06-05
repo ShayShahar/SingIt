56fe2230b6cd7000edff703186168dabf6e5c0ec
ShayShahar <shayscal90@gmail.com>
2016-06-05 22:17:23 +0300

    fix FavoritesViewActivity onResume

--- app/src/main/java/com/singit/shays/singit/FavoritesViewActivity.java
+++ app/src/main/java/com/singit/shays/singit/FavoritesViewActivity.java
@@ -101,5 +101,66 @@ public class FavoritesViewActivity extends AppCompatActivity {
         list.setOnItemClickListener(itemClickListener);
 
         }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        ArrayList<LyricsRes> favorites = dbHelper.get_favorite_songs();
+        adapter = new CustomAdapter(this,favorites);
+        list = (ListView) findViewById(R.id.favoritesList);
+        list.setEmptyView(findViewById(R.id.emptyFavorites));
+        list.setAdapter(adapter);
+
+        list.setOnItemLongClickListener( new AdapterView.OnItemLongClickListener() {
+            public boolean onItemLongClick(AdapterView<?> a, View v, final int position, long id) {
+                AlertDialog.Builder adb=new AlertDialog.Builder(FavoritesViewActivity.this);
+                adb.setTitle("Delete?");
+                LyricsRes item = (LyricsRes)list.getItemAtPosition(position);
+                adb.setMessage("Are you sure you want to delete " +item.artist+" "+item.title+"?" );
+                final int positionToRemove = position;
+                adb.setNegativeButton("Cancel", null);
+                adb.setPositiveButton("Ok", new AlertDialog.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        Log.d(TAG,"get in on click");
+                        LyricsRes item = (LyricsRes)list.getItemAtPosition(position);
+                        Log.d(TAG,"get the item"+item.toString());
+                        DBResult res = dbHelper.delete_song_from_favorites(item.id);
+                        Log.d(TAG,"the result from DB: "+res.toString());
+                        ArrayList<LyricsRes> refresh = dbHelper.get_favorite_songs();
+                        adapter = new CustomAdapter(getBaseContext(),refresh);
+                        list = (ListView) findViewById(R.id.favoritesList);
+                        list.setEmptyView(findViewById(R.id.emptyFavorites));
+                        list.setAdapter(adapter);                        Log.d(TAG,"notify");
+
+                    }});
+                adb.show();
+                return true;
+            }
+        });
+
+
+        AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View container, int position, long id) {
+
+                Log.d(TAG,"On item click..");
+                LyricsRes selected = (LyricsRes)list.getItemAtPosition(position);
+                LyricsRes pass = selected;
+                Log.d(TAG,selected.artist);
+                Log.d(TAG,Integer.toString(selected.id));
+                try{
+                    pass = api.getLyrics((LyricsRes)list.getItemAtPosition(position));
+                }catch(Exception e){
+                    Log.d(TAG,e.toString());
+                }
+                Intent intent = new Intent(FavoritesViewActivity.this,LyricsViewActivity.class);
+                intent.putExtra("view",pass);
+                startActivity(intent);
+                Log.d(TAG,"Call another activity");
+
+            }
+        };
+
+        list.setOnItemClickListener(itemClickListener);
+    }
 
     }
